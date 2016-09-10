66c25a661d1be4c25e568dc5d80f9fe08b471c81
dolare <dolarebrave@gmail.com>
2016-09-09 22:41:05 -0400

    Update LintCode_strStr.java

--- lintcode/LintCode_strStr.java
+++ lintcode/LintCode_strStr.java
@@ -1,33 +1,28 @@
-
-public class LintCode_strStr {
-	  public int strStr(String source, String target) {
+class Solution {
+    public int strStr(String source, String target) {
 	      // write your code here
-		  	   if(source == null || target == null){
-			   return -1;
-		  		}
-			   int slength = source.length();
-			   int tlength = target.length();
-			   
-			   if(source.equals(target) || target == "")
-				   return 0;		
-			
-		        for(int i = 0;i < slength - tlength;i++){
-		        	int index = i;
-		        	for(int j = 0;j < tlength;j++){
-		        		if(source.charAt(index++) != target.charAt(j)){
-		        			break;
-		        		}
-		        		if(j == tlength -1){
-		        			return i;
-		        		}
-		        	}
-		        }
-			   
-		        return -1;
-	  }
-	  
-	  public static void main(String[]  args){
-		  LintCode_strStr lint_strStr = new LintCode_strStr();
-		  System.out.println(lint_strStr.strStr(null, "a"));
+		  	 if(source == null || target == null){
+		  	     return -1;
+		  	 }
+		  	    
+		  	 int len1 = source.length();
+		  	 int len2 = target.length();
+		  	 
+		  	 if(len1 < len2){
+		  	     return -1;
+		  	 }
+		  	
+		  	 for(int i = 0;i < len1 - len2 + 1;i++){
+		  	     int j = 0;
+		  	     for(;j < len2;j++){
+		  	         if(source.charAt(i + j) != target.charAt(j)){
+		  	             break;
+		  	         }
+		  	     }
+		  	     if(j == len2){
+		  	        return i;
+		  	     }
+		  	 }
+		  	 return -1;
 	  }
 }
