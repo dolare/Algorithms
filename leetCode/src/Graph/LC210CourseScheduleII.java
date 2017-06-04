711b10425e793ae3e3532f2cbdf878412991474b
dolare <dolarebrave@gmail.com>
2017-06-04 12:17:22 -0400

    6/4_1

---
+++ leetCode/src/Graph/LC210CourseScheduleII.java
@@ -0,0 +1,45 @@
+package Graph;
+
+import java.util.*;
+
+/**
+ * Created by Dolare on 2017/5/25.
+ */
+public class LC210CourseScheduleII {
+    public static void main(String[] args){
+
+
+        int[][] test1 = {{0,1},{2,3},{3,1}};
+
+
+        //int[][] test2 = {{0,1},{1,2}};
+        //System.out.println(solution.canFinish(3,test2));
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if(numCourses <= 0 || prerequisites == null){
+            return null;
+        }
+
+        //create graph
+        Map<Integer,Set<Integer>> graph = new HashMap<>();
+
+        //initial grahp
+        for(int i = 0;i < prerequisites.length;i++){
+            graph.put(i,new HashSet<>());
+        }
+
+        for(int i = 0;i < prerequisites.length;i++){
+            graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+        //create a queue to do the bfs
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(Map.Entry<Integer,Set<Integer>> entry : graph.entrySet()){
+            if(entry.getValue().size() == 0){
+                
+            }
+        }
+    }
+
+}
